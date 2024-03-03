6593c5b7d7f79f90320c378eb66e2bb7a6ca1eec
Mustafa <mustafamasoud95@gmail.com>
2024-03-04 00:05:42 +0200

    Update popup.js

--- assets/data/popup.js
+++ assets/data/popup.js
@@ -2,18 +2,14 @@ const openPopupButton = document.getElementById("openPopupButton");
 const closePopupButton = document.getElementById("closePopupButton");
 const popup = document.getElementById("popup");
 
-// Function to open the popup
 function openPopup() {
 	popup.style.display = "block";
 }
 
-// Function to close the popup
 function closePopup() {
 	popup.style.display = "none";
 }
 
-// Event listener for opening the popup
 openPopupButton.addEventListener("click", openPopup);
 
-// Event listener for closing the popup
 closePopupButton.addEventListener("click", closePopup);
